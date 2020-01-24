d1cab5b2cb31688cc78a06ecfd01965ec2383f7f
leonardo-berlatto <leoberlatto@gmail.com>
2020-01-23 21:11:44 -0300

    :sparkles: Adding info cards

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   static final String defaultName = 'Leonardo Berlatto';
   static final String defaultJobTitle = 'Fullstak Developer';
   static final String defaultPhoneNumber = '+55 51 997879607';
+  static final String defaultEmail = 'leoberlatto@gamil.com';
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +41,40 @@ class MyApp extends StatelessWidget {
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal[700]),
+                    Container(
+                      margin: EdgeInsets.only(left: 8),
+                      child: Text(
+                        defaultPhoneNumber,
+                        style: TextStyle(color: Colors.teal[700]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.mail, color: Colors.teal[700]),
+                    Container(
+                      margin: EdgeInsets.only(left: 8),
+                      child: Text(
+                        defaultEmail,
+                        style: TextStyle(color: Colors.teal[700]),
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
