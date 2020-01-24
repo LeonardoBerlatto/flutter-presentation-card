4001134dc998b19a9cd349173a59b8fb6b1343da
leonardo-berlatto <leoberlatto@gmail.com>
2020-01-23 21:05:15 -0300

    :sparkles: Adding card picture, name and job title

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,45 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static final double radiusUnitsToACircle = 50.0;
+  static final String pathToProfilePicture = 'assets/profile_picture.jpeg';
+  static final String defaultName = 'Leonardo Berlatto';
+  static final String defaultJobTitle = 'Fullstak Developer';
+  static final String defaultPhoneNumber = '+55 51 997879607';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: radiusUnitsToACircle,
+                backgroundImage: AssetImage(pathToProfilePicture),
+              ),
+              Text(
+                defaultName,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                defaultJobTitle,
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
